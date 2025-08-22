3c5f95d45c3ce1bc3f1debbb6b493d61b591f0a2
DieroKreator <qtechwired@gmail.com>
2025-08-22 17:40:50 -0300

    feat: implement lifespan management for FastAPI application with logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from routers import aluno
 import logging
@@ -6,18 +7,28 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Código de inicialização da API
+    logger.info("API Iniciada")
+    
+    yield  # reprenta ação do tempo de vida da aplicação
+    
+    # Código de finalização da API
+    logger.info("API Finalizada")
+
 # Crio o objeto da API
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
-# Evento de inicialização da aplicação
-@app.on_event("startup")
-def startup_event():
-    logger.info("Aplicação iniciada com sucesso!")
+# # Evento de inicialização da aplicação (Old way)
+# @app.on_event("startup")
+# def startup_event():
+#     logger.info("Aplicação iniciada com sucesso!")
 
-# Evento de encerramento da aplicação
-@app.on_event("shutdown")
-def shutdown_event():
-    logger.info("Aplicação encerrada!")
+# # Evento de encerramento da aplicação (Old way)
+# @app.on_event("shutdown")
+# def shutdown_event():
+#     logger.info("Aplicação encerrada!")
 
 app.include_router(aluno.router, prefix="/buscar-aluno", tags=["Aluno"])
 
